d88814e9aaa5b55175c4413d9e9e9215b92f3a70
librenix <librenix@users.noreply.github.com>
2021-01-08 20:02:11 +0800

    Add cmd

--- main.go
+++ main.go
@@ -2,11 +2,41 @@ package main
 
 import (
 	"fmt"
+	"os"
 
-	_ "github.com/c-bata/go-prompt"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/librenix/paulared/ui"
+
+	"github.com/mattn/go-isatty"
 )
 
+var version = "0.1.0"
+
 func main() {
-	fmt.Println("Checking hardware ...")
-	Compatibility()
+	// Logging
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		fmt.Println("fatal: ", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	// Is cli or gui?
+	if isatty.IsTerminal(os.Stdout.Fd()) || isatty.IsCygwinTerminal(os.Stdout.Fd()) {
+		// Execute cli commands
+		if len(os.Args) > 1 {
+			Execute()
+		} else {
+			// Start TUI if without any args
+			fmt.Println(os.Args)
+			p := ui.Start()
+			if err := p.Start(); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+	} else {
+		// Qt GUI
+		//qt.Setup()
+	}
 }
